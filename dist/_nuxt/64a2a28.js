ee1854eefbe0b5f6bf2647d68624b6094dfd25ed
Saw Nerdar <sheep.blinker@gmail.com>
2023-05-01 18:11:58 +0630

    generated new dist

--- dist/_nuxt/64a2a28.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[18],{505:function(t,e,n){"use strict";n.r(e);var r={mounted:function(){this.$router.push("pages/"+this.$route.params.id)}},o=n(10),component=Object(o.a)(r,(function(){var t=this,e=t.$createElement;return(t._self._c||e)("div",[t._v("MID")])}),[],!1,null,null,null);e.default=component.exports}}]);
