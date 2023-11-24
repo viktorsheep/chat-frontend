47dd003df79010ba068609ad28e64e20087ebe09
Jupiter <jupitersatt001@protonmailcom>
2023-11-24 13:26:10 +0630

    ui: add loading on filter

---
+++ dist/_nuxt/6a2f04e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{522:function(n,e,t){"use strict";t.r(e);var l={layout:"app"},r=t(6),component=Object(r.a)(l,(function(){var n=this,e=n.$createElement;return(n._self._c||e)("div",[n._v("\n  "+n._s(n.$route.params)+"\n")])}),[],!1,null,null,null);e.default=component.exports}}]);
